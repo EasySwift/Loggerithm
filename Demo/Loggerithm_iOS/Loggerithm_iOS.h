fe5fe0c0c720fa7d1f30e83c156aa8cf22f1246a
stubbornnessness <1219721814yxj>
2016-10-11 23:16:09 +0800

    1.4版本开始支持carthage

---
+++ Demo/Loggerithm_iOS/Loggerithm_iOS.h
@@ -0,0 +1,19 @@
+//
+//  Loggerithm_iOS.h
+//  Loggerithm_iOS
+//
+//  Created by yuanxiaojun on 2016/10/11.
+//  Copyright © 2016年 HonghaoZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Loggerithm_iOS.
+FOUNDATION_EXPORT double Loggerithm_iOSVersionNumber;
+
+//! Project version string for Loggerithm_iOS.
+FOUNDATION_EXPORT const unsigned char Loggerithm_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Loggerithm_iOS/PublicHeader.h>
+
+
